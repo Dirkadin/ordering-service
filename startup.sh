771eefc5a6439349e7c8c1296415ab447e3aab96
Dirkadin <dirkadin@gmail.com>
2019-11-10 16:38:08 -0600

    x

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo Building image
 docker image build -t ordering-service .
-docker run -d -p 80:3000 --mount type=volume,source=hotburger-logs,target=/app/logs -it project2
+docker run -d -p 80:3000 --mount type=volume,source=hotburger-logs,target=/app/logs -it ordering-service
